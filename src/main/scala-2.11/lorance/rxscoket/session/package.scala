8a55b505f8f7ec4f3b29682142a4c2b0757d6a56
LoranceChen <wangchen@qiaobutang.com>
2016-03-25 23:35:23 +0800

    adds enCode method

--- src/main/scala-2.11/lorance/rxscoket/session/package.scala
+++ src/main/scala-2.11/lorance/rxscoket/session/package.scala
@@ -1,7 +1,15 @@
 package lorance.rxscoket
 
 import java.nio.ByteBuffer
+import lorance.rxscoket.session.implicitpkg._
 
 package object session {
   val EmptyByteBuffer = ByteBuffer.allocate(0)
+
+  def enCode(protoGroup: Byte, msg: String) = {
+    val msgBytes = msg.getBytes
+    val length = msgBytes.length.getByteArray
+    val bytes = protoGroup +: length
+    bytes ++ msgBytes
+  }
 }
