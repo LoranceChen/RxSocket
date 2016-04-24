e375c99480eb5ec504373402381ba27466380ec3
LoranceChen <wangchen@qiaobutang.com>
2016-04-24 14:52:11 +0800

    refine code

--- src/main/scala-2.11/lorance/rxscoket/presentation/json/JProtocol.scala
+++ src/main/scala-2.11/lorance/rxscoket/presentation/json/JProtocol.scala
@@ -7,11 +7,7 @@ import lorance.rxscoket._
 import lorance.rxscoket.session.{CompletedProto, ConnectedSocket}
 import lorance.rxscoket.session.implicitpkg._
 import net.liftweb.json.JsonAST.JValue
-import net.liftweb.json.JsonParser.ParseException
 import rx.lang.scala.Observable
-import rx.lang.scala.schedulers.ExecutionContextScheduler
-
-import scala.concurrent.ExecutionContext.Implicits._
 import scala.concurrent.duration.Duration
 import net.liftweb.json._
 
@@ -21,17 +17,12 @@ import net.liftweb.json._
 class JProtocol(connectedSocket: ConnectedSocket, read: Observable[Vector[CompletedProto]]) {
   val jRead = read.flatMap { cps =>
     val jsonProto = cps.filter(_.uuid == 1.toByte)
-    val x = Observable.from(
+    Observable.from(
       jsonProto.map{cp =>
-//        log("jread - ", -20)
         parseOpt(cp.loaded.array.string)}.
         filter(_.nonEmpty).
         map(_.get)
-//        map{x =>
-//          (x.get, try{Some((x.get \ "taskId").values.asInstanceOf[String] )} catch {case e : Throwable => None})}
-    )//.observeOn(ExecutionContextScheduler(global))//.onBackpressureBuffer//.publish
-//    x.connect
-    x
+    )
   }
 
   def send(any: Any) = {
@@ -59,29 +50,9 @@ class JProtocol(connectedSocket: ConnectedSocket, read: Observable[Vector[Comple
       * return: Observable[T]
       */
     def taskResult[T <: IdentityTask](taskId: String)
-                                      //additional: Option[Observable[T] => Observable[T]])
                                      (implicit mf: Manifest[T]) = {
-//      log(s"enter `taskResult` - $taskId", -15)
-
-      //todo publish could save for multi execute map body.
-      val pub = jRead.map { jsonProto =>
-        log(s"any JProtocol taskId - $taskId - $jsonProto - class - ${this}", 80)
-
-//        jsonProto._2 match {
-//          case Some(`taskId`) =>
-//            log(s"specify JProtocol taskId - $taskId, loaded - $jsonProto",-10 )
-//            try {
-//              Some(JsonParse.deCode[T](jsonProto._1))
-//            } catch {
-//              case e: MappingException =>
-//                log(s"find taskId but can't extract it, $jsonProto, to class - ${mf.runtimeClass}", 3)
-//                None
-//            }
-//          case _ =>
-//            log(s"other JProtocol taskId - $taskId, loaded - $jsonProto")
-//            None
-//        }
-
+      jRead.map { jsonProto =>
+        log(s"any JProtocol taskId - $taskId - $jsonProto - class - ${this}", 200)
         jsonProto \ "taskId" match {
           case JString(task) if task == taskId =>
             log(s"specify JProtocol taskId - $taskId, loaded - $jsonProto")
@@ -96,11 +67,9 @@ class JProtocol(connectedSocket: ConnectedSocket, read: Observable[Vector[Comple
             log(s"other JProtocol taskId - $taskId, loaded - $jsonProto")
             None
         }
-      }//.onBackpressureBuffer
-//      pub.connect
-      pub.filter(_.isDefined).map(_.get).
+      }.filter(_.isDefined).map(_.get).
         timeout(Duration(presentation.TIMEOUT, TimeUnit.SECONDS)).
-        doOnError { e => log(s"throw to JProtocol Obv - $taskId - $e", -17) }
+        doOnError { e => log(s"throw to JProtocol Obv - $taskId - $e", 1) }
     }
 
     val x = taskResult[Result](any.taskId)
