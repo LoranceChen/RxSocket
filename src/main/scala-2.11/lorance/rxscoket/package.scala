f851b573fb171607a4f2b198174eabdc889657e7
LoranceChen <wangchen@qiaobutang.com>
2016-04-24 18:49:04 +0800

    log with aim

--- src/main/scala-2.11/lorance/rxscoket/package.scala
+++ src/main/scala-2.11/lorance/rxscoket/package.scala
@@ -1,14 +1,18 @@
 package lorance
 
+import scala.collection.mutable
+
 package object rxscoket {
 
   // a threshold of level filter small.
   var logLevel = 0
 
+  var logAim = mutable.ListBuffer[String]()
   /**
     * @param level higher represent more detail message
     */
-  def log(msg: String, level: Int = 0): Unit = {
-    if (level <= this.logLevel) println(s"${Thread.currentThread.getName}:${System.currentTimeMillis()} - $msg")
+  def log(msg: String, level: Int = 0, aim: Option[String] = None): Unit = {
+    if (level <= this.logLevel || (aim.nonEmpty && logAim.contains(aim.get)))
+      println(s"${Thread.currentThread.getName}:${System.currentTimeMillis()} - ${aim.map(a => s"[$a] - ").getOrElse("")}$msg")
   }
 }
