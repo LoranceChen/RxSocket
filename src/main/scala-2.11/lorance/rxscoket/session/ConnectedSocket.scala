05a11a3b74b70510e3470a5a27ba0982a2a5f9d7
LoranceChen <wangchen@qiaobutang.com>
2016-04-27 20:11:48 +0800

    refine code

--- src/main/scala-2.11/lorance/rxscoket/session/ConnectedSocket.scala
+++ src/main/scala-2.11/lorance/rxscoket/session/ConnectedSocket.scala
@@ -3,7 +3,6 @@ package lorance.rxscoket.session
 import java.nio.ByteBuffer
 import java.nio.channels.{CompletionHandler, AsynchronousSocketChannel}
 import java.util.concurrent.Semaphore
-import java.util.concurrent.locks.{ReentrantLock, Lock}
 
 import lorance.rxscoket.session.exception.ReadResultNegativeException
 import lorance.rxscoket._
@@ -15,8 +14,6 @@ import scala.collection.mutable
 import scala.concurrent.{Future, Promise}
 import scala.util.{Success, Failure}
 import scala.concurrent.ExecutionContext.Implicits.global
-import lorance.rxscoket.session.execution.customExecutionContent
-//import lorance.rxscoket.session.execution.currentThread
 
 class ConnectedSocket(val socketChannel: AsynchronousSocketChannel) {
   private val readerDispatch = new ReaderDispatch()
@@ -25,71 +22,9 @@ class ConnectedSocket(val socketChannel: AsynchronousSocketChannel) {
   private def append(s: Subscriber[Vector[CompletedProto]]) = readSubscribes.synchronized(readSubscribes += s)
   private def remove(s: Subscriber[Vector[CompletedProto]]) = readSubscribes.synchronized(readSubscribes -= s)
 
-  //locks for send msg
-//  val sendGetLock = new AnyRef
-//  val sendPutLock = new AnyRef
-
-//  val msgQueue = new MessageQueue[(ByteBuffer, Promise[Unit])](
-//    Configration.NET_SENDMSG_OVERLOAD,
-//    sendPutLock.synchronized(sendPutLock.notify()), //    beginGet: => Unit,
-//    log("able to put net data to msg queue again"),//sendBeginPutLock.notify(), //  beginPut: => Unit,
-//    throw new Exception("ready send message to lot"),//sendBlockPutLock.wait(),   //  blockPut: => Unit,
-//    sendPutLock.synchronized(sendPutLock.wait())    //  blockGet: => Unit)
-//  )
-
-  /**
-    * if put logic in new Thread() body, it will use new operation thread to init
-    */
-//  val sendQueueMsg = new Thread {
-//    override def run(): Unit = {
-//      log("sendQueueMsg thread name test - ", -100)
-//      val curThread = currentThread
-//      def sendLoop(): Unit = {
-//        sendPutLock.synchronized {
-//          msgQueue.get match {
-//            case None => sendLoop()
-//            case Some(msg) =>
-//              sendMsg(msg._1).onComplete {
-//                case Success(rsp) =>
-//                  log("sendQueueMsg thread name should equal uppers - ", -100)
-//                  msg._2.trySuccess(rsp)
-//                  sendLoop()
-//                case Failure(f) => throw f
-//              }(curThread)//todo it not work
-//          }
-//        }
-//      }
-//      sendLoop()
-//    }
-//  }
-
-//  sendQueueMsg.start()
-
-  /**
-    * if queue has space return true
-    *
-    * @return also consider as Unit because TCP can ensure arrive
-    *         todo test server disconnect does will write success? if also success the result is meanless. we also should use addition operation ensure arrive success.
-    */
-//  def send(msg: ByteBuffer) = {
-//    val p = Promise[Unit]
-//    msgQueue.put(msg, p)
-////    match {
-////      case None => throw new Exception("ready send message to lot")
-////      case Some(_) => Unit
-////    }
-//    p.future
-//  }
-
   val netMsgCountBuf = new Count()
 
   val readAttach = Attachment(ByteBuffer.allocate(Configration.READBUFFER_LIMIT), socketChannel)
-
-  //val readLocker = new AnyRef
-
-  val dispatchThread = customExecutionContent(4)//used to wait
-
-  //def continueRead(): Unit = readLocker.notify()
 
   def disconnect(): Unit = socketChannel.close()
 
@@ -123,35 +58,22 @@ class ConnectedSocket(val socketChannel: AsynchronousSocketChannel) {
           readerDispatch.receive(src).foreach{protos =>
             log(s"dispatched protos - ${protos.map(p => p.loaded.array().string)}", 70, Some("dispatch-protos"))
             netMsgCountBuf.add(protos.map(_.loaded.capacity).sum)
-//            log(s"CompletedPtoro buffered count - ${netMsgCountBuf.get}", -1000)
             for (s <- readSubscribes) s.onNext(protos)
           }
-
-          //limit read operation
-//          if(netMsgCountBuf.get >= Configration.NET_MSG_OVERLOAD) {
-//            log(s"hold read operation - ")
-//            readLocker.wait()
-//            beginReadingClosure
-//          } else {
-//          log(s"receive sleeping - ", -100)
-//          Thread.sleep(Configration.NET_RATE)
           beginReadingClosure()
-//          }
-      }//(dispatchThread)
+      }
     }
     beginReadingClosure()
   }
 
-  val count = new Count()
+  private lazy val count = new Count()
 
-  val writeSemaphore = new Semaphore(1)
+  private val writeSemaphore = new Semaphore(1)
+
   /**
     * it seems NOT support concurrent write, but NOT break reading.
     * after many times test, later write request will be ignored when
     * under construct some write operation.
-    *
-    * use message queue to deal with send message, send message will
-    * this method not support multi-thread
     *
     *  @throw WritePendingException Unchecked exception thrown when an attempt is made to write to an asynchronous socket channel and a previous write has not completed.
     */
@@ -173,8 +95,8 @@ class ConnectedSocket(val socketChannel: AsynchronousSocketChannel) {
         p.tryFailure(exc)
       }
     })
+
     p.future
-//    }
   }
 
   private def read(readAttach: Attachment): Future[Attachment] = {
