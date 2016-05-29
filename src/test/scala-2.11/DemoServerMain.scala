e492029d717451b5d6a4d740674c608a9ecccd07
LoranceChen <wangchen@qiaobutang.com>
2016-05-29 21:46:07 +0800

    update readme and Demo

--- src/test/scala-2.11/DemoServerMain.scala
+++ src/test/scala-2.11/DemoServerMain.scala
@@ -5,11 +5,7 @@ import lorance.rxscoket._
 import lorance.rxscoket.session.{ConnectedSocket, ServerEntrance}
 import rx.lang.scala.Observable
 
-/**
-  * TODO difference between Observer and Subscription? both of them has OnNext, OnError, OnComplete.which stage in Rx is them place
-  */
 object DemoServerMain extends App {
-  logLevel = 1000
   val server = new ServerEntrance("localhost", 10002)
   val socket: Observable[ConnectedSocket] = server.listen
 
@@ -27,19 +23,16 @@ object DemoServerMain extends App {
 
   socket.subscribe(s => log(s"Hi, John2, someone connected - "))
 
-  //this way also make a subscribe event to hot Observable by explicit create new one. It seems odd but practical.
-//  var read2: Observable[Vector[CompletedProto]] = {
-//    val p = Promise[Observable[Vector[CompletedProto]]]
-//    socket.subscribe{l => log("read start reading");p.trySuccess(l.startReading)}
-//    Observable.from(p.future).flatten
-//  }
-
   read.subscribe{r => r._2.foreach{x =>
-      log(s"first subscriber get protocol - ${new String(x.loaded.array())}")
-      val msg = "hi client~"
+      val clientInfo = new String(x.loaded.array())
+      log(s"first subscriber get protocol - $clientInfo")
+
+      val msg = "hi client~ I get your message - " + clientInfo
+      log("response to client when get his msg - " + msg)
       r._1.send(ByteBuffer.wrap(session.enCode(0.toByte, msg)))
     }
   }
+
   read.subscribe{r => r._2.foreach{x => log(s"second subscriber get protocol - ${new String(x.loaded.array())}")}}
 
   Thread.currentThread().join()
