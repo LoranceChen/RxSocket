7b8c31d844dc440df1df514693874b0eef79ac03
LoranceChen <w68055010@qq.com>
2018-05-07 22:04:16 +0800

    limit block execute context to minimize scope

--- src/main/scala/lorance/rxsocket/session/implicitpkg/FutureEx.scala
+++ src/main/scala/lorance/rxsocket/session/implicitpkg/FutureEx.scala
@@ -1,27 +1,35 @@
 package lorance.rxsocket.session.implicitpkg
 
-//import rxsocket.rxsocketLogger
+import lorance.rxsocket.session.execution
 
 import scala.concurrent.duration.Duration
-import scala.concurrent.{ExecutionContext, Future, Promise, blocking}
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 sealed class FutureTimeoutException extends RuntimeException
 sealed class FutureTimeoutNotOccur extends RuntimeException
 
 class FutureEx[T](f: Future[T]) {
-  def withTimeout(ms: Long = 2000)(implicit executor: ExecutionContext): Future[T] = Future.firstCompletedOf(List(f, {
-    val p = Promise[T]
-    Future {
-      blocking(Thread.sleep(ms))
-      if(!f.isCompleted) {
-//        rxsocketLogger.log(s"[Throw] - FutureTimeoutException after - ${ms}ms", 15)
-        p.tryFailure(new FutureTimeoutException)
-      } else {
-        p.tryFailure(new FutureTimeoutNotOccur)
-      }
-    }
-    p.future
-  }))
+  // use a waitExecutor to handle sleep
+  def withTimeout(ms: Long = 2000): Future[T] = {
+    val ex: ExecutionContextExecutor = execution.waitExecutor
 
-  def withTimeout(duration: Duration)(implicit executor: ExecutionContext): Future[T] = withTimeout(duration.toMillis)
+    Future.firstCompletedOf(List(f, {
+      //todo 看看monix库中有类似Cancel Task的东西能否优化这里的Thread.sleep
+      val p = Promise[T]
+      Future {
+        blocking(Thread.sleep(ms))
+        if(!f.isCompleted) {
+          p.tryFailure(new FutureTimeoutException)
+        } else {
+          p.tryFailure(new FutureTimeoutNotOccur)
+        }
+      }(ex)
+      p.future
+    }))
+  }
+
+  def withTimeout(duration: Duration)
+//                 (implicit executor: ExecutionContext)
+                  : Future[T] = withTimeout(duration.toMillis)
 }
